a4746b376cfe936880e429aba578da08b27d6d39
tlx <tianlixin@neucloud.cn>
2019-08-01 20:56:27 +0800

    整合vue

---
+++ src/components/project/moduleList.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul class="moduleList">
+    <li draggable="true" data-name="ps1">ps1</li>
+    <li draggable="true" data-name="ps2">ps2</li>
+    <li draggable="true" data-name="ps3">ps3</li>
+    <li draggable="true" data-name="ps4">ps4</li>
+    <li draggable="true" data-name="switch">switch</li>
+  </ul>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
---
+++ src/components/project/propList.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>右侧模块属性</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
